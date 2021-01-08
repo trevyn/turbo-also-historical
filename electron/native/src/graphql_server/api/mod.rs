98112fded4a4d756ff665caea34352747c8b71d3
Eden <github@eden2.com>
2021-01-08 12:31:59 +0400

    refactor into api

---
+++ electron/native/src/graphql_server/api/mod.rs
@@ -0,0 +1,46 @@
+use futures::Stream;
+use juniper::{graphql_object, graphql_subscription, FieldError, FieldResult};
+use std::{convert::TryInto, pin::Pin, time::Duration};
+
+mod datastore;
+use datastore::Pdf;
+
+pub struct Query;
+
+#[graphql_object]
+impl Query {
+ async fn list_pdfs() -> FieldResult<Vec<Pdf>> {
+  Ok(datastore::list_pdfs()?)
+ }
+}
+
+pub struct Mutation;
+
+#[graphql_object]
+impl Mutation {
+ async fn add_pdf(content: String) -> FieldResult<Pdf> {
+  datastore::add_pdf(&content)?;
+  Ok(Pdf {
+   id: Some(content.len().try_into()?),
+   name: Some("from addPdf".into()),
+   ..Default::default()
+  })
+ }
+}
+
+type UsersStream = Pin<Box<dyn Stream<Item = Result<Pdf, FieldError>> + Send>>;
+
+pub struct Subscription;
+
+#[graphql_subscription]
+impl Subscription {
+ async fn users_subscription() -> UsersStream {
+  let mut counter = 0;
+  let stream = tokio::time::interval(Duration::from_secs(5)).map(move |_| {
+   counter += 1;
+   Ok(Pdf { id: Some(counter), name: Some("stream user".to_string()), ..Default::default() })
+  });
+
+  Box::pin(stream)
+ }
+}
--- electron/native/src/graphql_server/mod.rs
+++ electron/native/src/graphql_server/mod.rs
@@ -1,57 +1,15 @@
-use futures::{FutureExt as _, Stream};
-use juniper::{graphql_object, graphql_subscription, FieldError, FieldResult, RootNode};
+use futures::FutureExt as _;
 use juniper_graphql_ws::ConnectionConfig;
 use juniper_warp::{playground_filter, subscriptions::serve_graphql_ws};
-use std::convert::TryInto;
-use std::{env, pin::Pin, sync::Arc, time::Duration};
+use std::{env, sync::Arc};
 use warp::{http, Filter};
 
-mod datastore;
-use datastore::Pdf;
+mod api;
+use api::{Mutation, Query, Subscription};
 
 mod asset_server;
 
-struct Query;
-
-#[graphql_object]
-impl Query {
- async fn list_pdfs() -> FieldResult<Vec<Pdf>> {
-  Ok(datastore::list_pdfs()?)
- }
-}
-
-struct Mutation;
-
-#[graphql_object]
-impl Mutation {
- async fn add_pdf(content: String) -> FieldResult<Pdf> {
-  datastore::add_pdf(&content)?;
-  Ok(Pdf {
-   id: Some(content.len().try_into()?),
-   name: Some("from addPdf".into()),
-   ..Default::default()
-  })
- }
-}
-
-type UsersStream = Pin<Box<dyn Stream<Item = Result<Pdf, FieldError>> + Send>>;
-
-struct Subscription;
-
-#[graphql_subscription]
-impl Subscription {
- async fn users_subscription() -> UsersStream {
-  let mut counter = 0;
-  let stream = tokio::time::interval(Duration::from_secs(5)).map(move |_| {
-   counter += 1;
-   Ok(Pdf { id: Some(counter), name: Some("stream user".to_string()), ..Default::default() })
-  });
-
-  Box::pin(stream)
- }
-}
-
-type Schema = RootNode<'static, Query, Mutation, Subscription>;
+type Schema = juniper::RootNode<'static, Query, Mutation, Subscription>;
 
 fn schema() -> Schema {
  Schema::new(Query, Mutation, Subscription)
