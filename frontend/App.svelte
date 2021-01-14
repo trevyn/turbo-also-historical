28b8fb886d337f564e7f7a7385ddc5e0fc13f5d9
Eden <github@eden2.com>
2021-01-14 21:52:14 +0400

    urql/svelte queries and mutations work!

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,5 +1,37 @@
 <!-- App.svelte -->
 <script lang="ts">
+ import { initClient, mutation } from "@urql/svelte";
+ initClient({ url: "http://localhost:8080/graphql" });
+
+ import { operationStore, query } from "@urql/svelte";
+ const listPdfs = operationStore(`
+    query {
+      listPdfs {
+        rowid
+        name
+      }
+    }
+  `);
+ query(listPdfs);
+
+ const addPdf = operationStore(`
+mutation addPdf($content: String!) {
+ addPdf(content: $content)
+ {
+  rowid
+  id
+  filesize
+  name
+  content
+ }
+}
+  `);
+
+ const mutateTodo = mutation(addPdf);
+ function updateTodo(newTitle) {
+  mutateTodo({ content: newTitle });
+ }
+
  import { onMount } from "svelte";
  let count = 0; // @hmr:keep
  onMount(() => {
@@ -17,6 +49,23 @@
    <code>{count}</code>
    seconds.
   </p>
+
+  <button
+   on:click={() => {
+    mutateTodo({ content: 'NEW PDF' });
+   }}>BUTTON</button>
+
+  {#if $listPdfs.fetching}
+   <p>Loading...</p>
+  {:else if $listPdfs.error}
+   <p>Oh no... {$listPdfs.error.message}</p>
+  {:else}
+   <ul>
+    {#each $listPdfs.data.listPdfs as todo}
+     <li>{JSON.stringify(todo)}</li>
+    {/each}
+   </ul>
+  {/if}
 
   <a
    class="App-link"
