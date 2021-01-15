67d4febd7ca979cbc27fc4c2d958ff3ec5558d17
Eden <github@eden2.com>
2021-01-16 01:27:47 +0400

    fun with dragging

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -74,22 +74,16 @@ subscription {
   mutateTodo({ content: newTitle });
  }
 
- import { onMount } from "svelte";
- let count = 0; // @hmr:keep
- onMount(() => {
-  const interval = setInterval(() => count++, 1000);
-  return () => {
-   clearInterval(interval);
-  };
- });
+ let m = { x: 0, y: 0 };
+
+ function handleMousemove(event) {
+  m.x = event.clientX;
+  m.y = event.clientY;
+ }
 </script>
 
-<div class="App">
- <p class="text-yellow-500 font-extrabold">
-  Page has been here for
-  <code>{count}</code>
-  seconds.
- </p>
+<main style="width:100%; height:100vh" on:mousemove={handleMousemove}>
+ <div>The mouse position is {m.x - 300} x {m.y - 300}</div>
 
  {#if !$usersSubscription.data}
   <p>No new messages</p>
@@ -117,4 +111,22 @@ subscription {
    {/each}
   </ul>
  {/if}
-</div>
+
+ <div
+  class="m-10 absolute rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-gray-400 max-w-md"
+  style="left:{m.x - 100}px;top:{m.y - 100}px;">
+  <div class="flex-shrink-0">
+   <img
+    class="h-20 w-20 rounded-full"
+    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+    alt="" />
+  </div>
+  <div class="min-w-0">
+   <a href="#" class="focus:outline-none">
+    <span class="absolute inset-0" aria-hidden="true" />
+    <p class="text-2xl font-medium text-gray-700">Leslie Alexander</p>
+    <p class="text-2xl text-gray-400 truncate">Co-Founder / CEO</p>
+   </a>
+  </div>
+ </div>
+</main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+ purge: [],
+ darkMode: false, // or 'media' or 'class'
+ theme: {
+  extend: {},
+ },
+ variants: {
+  extend: {},
+ },
+ plugins: [
+  require("@tailwindcss/forms"),
+  require("@tailwindcss/typography"),
+  require("@tailwindcss/aspect-ratio"),
+ ],
+};
