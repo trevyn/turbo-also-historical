460209386301197ca8d2250191fa065c1b5d54ee
Eden <github@eden2.com>
2021-01-16 22:32:06 +0400

    don't try to snowpack typescript-urql-svelte

--- frontend/snowpack.config.js
+++ frontend/snowpack.config.js
@@ -8,7 +8,12 @@ module.exports = {
   "@snowpack/plugin-svelte",
   "@snowpack/plugin-postcss",
  ],
- exclude: ["**/node_modules/**/*", "queries-plugin.js", "run-codegen.mjs"],
+ exclude: [
+  "**/node_modules/**/*",
+  "queries-plugin.js",
+  "run-codegen.mjs",
+  "**/typescript-urql-svelte/**/*",
+ ],
  routes: [
   /* Enable an SPA Fallback in development: */
   // {"match": "routes", "src": ".*", "dest": "/index.html"},
