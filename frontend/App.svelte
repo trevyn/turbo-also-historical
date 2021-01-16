45fe80fa5cd05fd9d381aedc44e2588fbcdad623
Eden <github@eden2.com>
2021-01-16 12:00:39 +0400

    factor into graphql_ops.svelte works

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,17 +1,17 @@
-<!-- App.svelte -->
 <script>
  import {
   initClient,
   defaultExchanges,
   subscriptionExchange,
-  query,
-  mutation,
-  subscription,
-  operationStore,
  } from "@urql/svelte";
+ import { devtoolsExchange } from "@urql/devtools";
  import { SubscriptionClient } from "subscriptions-transport-ws";
- import White from "./c/White.svelte";
- import Alien from "./c/Alien.svelte";
+
+ import {
+  listPdfsQuery,
+  addPdfMutation,
+  usersSubscriptionSubscription,
+ } from "./graphql_ops.svelte";
 
  const subscriptionClient = new SubscriptionClient(
   "ws://localhost:8080/subscriptions",
@@ -23,7 +23,7 @@
  initClient({
   url: "http://localhost:8080/graphql",
   exchanges: [
-   // devtoolsExchange,
+   devtoolsExchange,
    ...defaultExchanges,
    subscriptionExchange({
     forwardSubscription(operation) {
@@ -33,89 +33,37 @@
   ],
  });
 
- const listPdfs = operationStore(`
-    query {
-      listPdfs {
-        rowid
-        name
-      }
-    }
-  `);
- query(listPdfs);
-
- const addPdf = operationStore(`
-mutation addPdf($content: String!) {
- addPdf(content: $content)
- {
-  rowid
-  id
-  filesize
-  name
-  content
- }
-}
-  `);
-
- const usersSubscription = operationStore(`
-subscription {
- usersSubscription
- {
-  id
- }
-}
-  `);
-
- const handleSubscription = (messages = [], data) => {
-  console.log(data);
-  return [data.usersSubscription, ...messages];
- };
- subscription(usersSubscription, handleSubscription);
-
- const mutateTodo = mutation(addPdf);
- function updateTodo(newTitle) {
-  mutateTodo({ content: newTitle });
- }
-
- let m = { x: 300, y: 300 };
-
- function handleMousemove(event) {
-  m.x = event.clientX;
-  m.y = event.clientY;
- }
+ const listPdfs = listPdfsQuery();
+ const addPdf = addPdfMutation();
+ const usersSubscription = usersSubscriptionSubscription(
+  (messages = [], data) => [data.usersSubscription, ...messages]
+ );
 </script>
 
-<main style="width:100%; height:100vh" on:mousemove={handleMousemove}>
- <div>The mouse position is {m.x - 300} x {m.y - 300}</div>
+{#if !$usersSubscription.data}
+ <p>No new messages</p>
+{:else}
+ <ul>
+  {#each $usersSubscription.data as message}
+   <li>{JSON.stringify(message)}</li>
+  {/each}
+ </ul>
+{/if}
 
- {#if !$usersSubscription.data}
-  <p>No new messages</p>
- {:else}
-  <ul>
-   {#each $usersSubscription.data as message}
-    <li>{JSON.stringify(message)}</li>
-   {/each}
-  </ul>
- {/if}
+<button
+ on:click={() => {
+  addPdf({ content: 'NEW PDF' });
+  $listPdfs.context = { requestPolicy: 'cache-and-network', forceUpdate: Date.now() };
+ }}>BUTTON</button>
 
- <button
-  on:click={() => {
-   mutateTodo({ content: 'NEW PDF' });
-  }}>BUTTON</button>
-
- {#if $listPdfs.fetching}
-  <p>Loading...</p>
- {:else if $listPdfs.error}
-  <p>Oh no... {$listPdfs.error.message}</p>
- {:else}
-  <ul>
-   {#each $listPdfs.data.listPdfs as todo}
-    <li>{JSON.stringify(todo)}</li>
-   {/each}
-  </ul>
- {/if}
-
- {#if m.x > 50 && m.y > 50}
-  <White {m} />
-  <Alien {m} />
- {/if}
-</main>
+{#if $listPdfs.fetching}
+ <p>Loading...</p>
+{:else if $listPdfs.error}
+ <p>Oh no... {$listPdfs.error.message}</p>
+{:else}
+ <ul>
+  {#each $listPdfs.data.listPdfs as todo}
+   <li>{JSON.stringify(todo)}</li>
+  {/each}
+ </ul>
+{/if}
---
+++ frontend/graphql_ops.svelte
@@ -0,0 +1,46 @@
+<script context="module">
+ import { operationStore, query, mutation, subscription } from "@urql/svelte";
+
+ const listPdfsDocument = `
+  query {
+   listPdfs {
+    rowid
+    name
+   }
+  }
+ `;
+
+ const addPdfDocument = `
+  mutation addPdf($content: String!) {
+   addPdf(content: $content)
+   {
+    rowid
+    id
+    filesize
+    name
+    content
+   }
+  }
+ `;
+
+ const usersSubscriptionDocument = `
+  subscription {
+   usersSubscription
+   {
+    id
+   }
+  }
+ `;
+
+ export function listPdfsQuery() {
+  return query(operationStore(listPdfsDocument));
+ }
+
+ export function addPdfMutation() {
+  return mutation(operationStore(addPdfDocument));
+ }
+
+ export function usersSubscriptionSubscription(handler) {
+  return subscription(operationStore(usersSubscriptionDocument), handler);
+ }
+</script>
