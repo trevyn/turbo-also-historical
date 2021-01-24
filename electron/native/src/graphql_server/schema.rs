e8acc2589e5fa63d125e737a4e8f7bf1b35146e2
Eden <github@eden2.com>
2021-01-24 18:09:48 +0400

    pdf to card part 1

--- electron/native/src/graphql_server/schema.rs
+++ electron/native/src/graphql_server/schema.rs
@@ -1,11 +1,17 @@
 use futures::Stream;
 use i54_::i54;
-use juniper::{graphql_object, graphql_subscription, FieldError, FieldResult};
+use juniper::{graphql_object, graphql_subscription, ExecutionResult, FieldError, FieldResult};
 use std::{convert::TryInto, pin::Pin, time::Duration};
 use turbosql::{execute, select};
 
-mod datastore;
-use datastore::Pdf;
+#[derive(juniper::GraphQLObject, turbosql::Turbosql, Default, Debug)]
+pub struct Card {
+ pub rowid: Option<i54>,
+ pub id: Option<i32>,
+ pub filesize: Option<i54>,
+ pub name: Option<String>,
+ pub content: Option<String>,
+}
 
 type Schema = juniper::RootNode<'static, Query, Mutation, Subscription>;
 
@@ -19,65 +25,65 @@ pub struct Query;
 #[fold_impls::fold_impls]
 fn _query_impls() {
  #[derive(juniper::GraphQLObject, Debug)]
- struct ListPdfsResultItem {
+ struct ShortCard {
   rowid: i54,
   name: String,
  }
 
  #[graphql_object]
  impl Query {
-  async fn list_pdfs() -> FieldResult<Vec<ListPdfsResultItem>> {
-   Ok(dbg!(select!(Vec<ListPdfsResultItem> "rowid, name FROM pdf"))?)
+  async fn list_cards_short() -> FieldResult<Vec<ShortCard>> {
+   Ok(dbg!(select!(Vec<ShortCard> "rowid, name FROM card"))?)
   }
  }
 
- #[derive(juniper::GraphQLObject, Debug)]
- struct ListPdfsResultItem2 {
-  rowid: i54,
-  name: String,
- }
-
  #[graphql_object]
  impl Query {
-  async fn list_pdfs2() -> FieldResult<Vec<ListPdfsResultItem2>> {
-   Ok(dbg!(select!(Vec<ListPdfsResultItem2> "rowid, name FROM pdf"))?)
+  async fn list_cards_full() -> FieldResult<Vec<Card>> {
+   Ok(dbg!(select!(Vec<Card>))?)
   }
  }
 }
 
 pub struct Mutation;
 
-#[derive(juniper::GraphQLObject, Debug)]
-struct MutationResult {
- success: bool,
-}
-
 #[graphql_object]
 impl Mutation {
- async fn add_pdf(content: String) -> FieldResult<Pdf> {
-  datastore::add_pdf(&content)?;
-  Ok(Pdf {
-   id: Some(content.len().try_into()?),
-   name: Some("from addPdf".into()),
+ async fn add_card(content: String) -> FieldResult<Card> {
+  let card = Card {
+   content: Some(content.to_string()),
+   name: Some(format!("a card of {} bytes", content.len())),
+   filesize: Some(content.len().try_into()?),
    ..Default::default()
-  })
+  };
+
+  card.insert()?;
+
+  Ok(card)
  }
- async fn delete_pdf(rowid: i54) -> MutationResult {
-  MutationResult { success: execute!("DELETE FROM pdf WHERE rowid = ?", rowid).is_ok() }
+
+ async fn update_card(rowid: i54, content: String) -> FieldResult<Card> {
+  execute!("UPDATE card SET content = ? WHERE rowid = ?", content, rowid)?;
+  Ok(select!(Card "WHERE rowid = ?", rowid)?)
+ }
+
+ async fn delete_card(rowid: i54) -> FieldResult<bool> {
+  execute!("DELETE FROM card WHERE rowid = ?", rowid)?;
+  Ok(true)
  }
 }
 
-type UsersStream = Pin<Box<dyn Stream<Item = Result<Pdf, FieldError>> + Send>>;
+type CardStream = Pin<Box<dyn Stream<Item = Result<Card, FieldError>> + Send>>;
 
 pub struct Subscription;
 
 #[graphql_subscription]
 impl Subscription {
- async fn users_stream() -> UsersStream {
+ async fn card_stream() -> CardStream {
   let mut counter = 0;
   let stream = tokio::time::interval(Duration::from_secs(5)).map(move |_| {
    counter += 1;
-   Ok(Pdf { id: Some(counter), name: Some("stream user".to_string()), ..Default::default() })
+   Ok(Card { id: Some(counter), name: Some("stream user".to_string()), ..Default::default() })
   });
 
   Box::pin(stream)
