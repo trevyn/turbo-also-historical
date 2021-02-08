adc303facf4c3e46d31d14ced6d472afc877b11d
Eden <github@eden2.com>
2021-02-08 15:42:28 +0400

    move prosemirror-collab-server.js into src

--- prosemirror_collab_server/esbuild-run.js
+++ prosemirror_collab_server/esbuild-run.js
@@ -5,14 +5,14 @@ let onResolvePlugin = {
 
   // Redirect all paths starting with "frontend/" to "../frontend/"
   build.onResolve({ filter: /^frontend\// }, args => {
-   return { path: path.join(args.resolveDir, "../", args.path) };
+   return { path: path.join(args.resolveDir, "../../", args.path) };
   });
  },
 };
 
 require("esbuild")
  .build({
-  entryPoints: ["prosemirror-collab-server.js"],
+  entryPoints: ["src/prosemirror-collab-server.js"],
   bundle: true,
   outfile: "build/prosemirror-collab-server.js",
   plugins: [onResolvePlugin],
