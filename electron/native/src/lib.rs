3817322a997774f3efcd41a51d40bcd0997cab45
Eden <github@eden2.com>
2021-02-10 19:41:23 +0400

    wip 3 insane futures thing

--- electron/native/src/lib.rs
+++ electron/native/src/lib.rs
@@ -2,12 +2,21 @@ use futures::prelude::*;
 use neon::event::EventHandler;
 use neon::prelude::*;
 use once_cell::sync::Lazy;
-use std::{future::Future, pin::Pin, sync::Mutex, task::Poll};
+use std::{
+ collections::HashMap,
+ future::Future,
+ pin::Pin,
+ sync::Mutex,
+ task::{Poll, Waker},
+};
 
 #[allow(clippy::type_complexity)]
 static PROSEMIRROR_CALLBACK: Lazy<
  Mutex<Option<Box<dyn Fn(String, String) -> Pin<Box<dyn Future<Output = String>>> + Send>>>,
 > = Lazy::new(|| Mutex::new(None));
+
+static RESULTWAKERS: Lazy<Mutex<HashMap<i32, ResultWakers>>> =
+ Lazy::new(|| Mutex::new(HashMap::new()));
 
 #[allow(clippy::unnecessary_wraps)]
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
@@ -27,7 +36,7 @@ async fn do_thing() {
 }
 
 fn my_fut() -> Pin<Box<dyn Future<Output = String>>> {
- Box::pin(MyFut)
+ Box::pin(MyFut { slot: 0 })
 }
 
 #[allow(clippy::unnecessary_wraps)]
@@ -35,13 +44,36 @@ fn helper(mut cx: FunctionContext) -> JsResult<JsUndefined> {
  Ok(cx.undefined())
 }
 
-struct MyFut;
+#[derive(Debug)]
+struct ResultWakers {
+ result: Option<String>,
+ waker: Option<std::task::Waker>,
+}
+
+struct MyFut {
+ slot: i32,
+ // result: Option<String>,
+ // waker: Option<std::task::Waker>,
+ // closure: Option<Box<dyn FnMut(String) + Send>>,
+}
 
 impl Future for MyFut {
  type Output = String;
  fn poll(self: Pin<&mut Self>, cx: &mut core::task::Context) -> Poll<Self::Output> {
-  cx.waker();
-  Poll::Ready("42".to_string())
+  let mut resultwakers = RESULTWAKERS.lock().unwrap();
+  let r = resultwakers.get_mut(&self.slot).unwrap().result.take();
+
+  match r {
+   None => {
+    let waker: Waker = cx.waker().clone();
+    resultwakers.get_mut(&self.slot).unwrap().waker = Some(waker);
+    Poll::Pending
+   }
+   Some(r) => {
+    resultwakers.remove(&self.slot);
+    Poll::Ready(r)
+   }
+  }
  }
 }
 
@@ -55,16 +87,43 @@ fn register_prosemirror_apply_callback(mut cx: FunctionContext) -> JsResult<JsUn
  let execute = move |old: String, patch: String| -> Pin<Box<dyn Future<Output = String>>> {
   // let mut output: Option<String> = None;
 
+  // let myfut = futs.get_mut(&slot).unwrap();
+
+  let mut resultwakers = RESULTWAKERS.lock().unwrap();
+  let slot = (1..100).find(|n| !resultwakers.contains_key(n)).unwrap();
+  let fut = MyFut { slot };
+  assert!(resultwakers.insert(slot, ResultWakers { result: None, waker: None }).is_none());
+
+  // let myfut = futs.get_mut(&slot).unwrap();
+
+  // let closure = Box::new(|result: String| {
+  //  let mut futs = FUTS.lock().unwrap();
+  //  let slot = (1..100).find(|n| !futs.contains_key(n)).unwrap();
+  //  let myfut = *futs.get_mut(&slot).unwrap();
+  //  myfut.result = Some(result);
+  //  let waker = myfut.waker.take();
+  //  if let Some(w) = waker {
+  //   w.wake()
+  //  }
+  // });
+
+  // fut.closure = Some(closure);
+
   cb.schedule(move |cx: &mut neon::context::TaskContext| {
    eprintln!("in scheduled");
-   let success = neon::types::JsFunction::new(cx, helper).unwrap();
+   let helper = neon::types::JsFunction::new(cx, helper).unwrap();
 
-   let args: Vec<Handle<JsValue>> =
-    vec![success.upcast(), cx.string(old).upcast(), cx.string(patch).upcast()];
+   let args: Vec<Handle<JsValue>> = vec![
+    helper.upcast(),
+    // cx.number(slot).upcast(),
+    cx.string(old).upcast(),
+    cx.string(patch).upcast(),
+   ];
    args
   });
 
-  Box::pin(MyFut)
+  Box::pin(fut)
+  // Box::pin(MyFut { slot: 0, result: None })
  };
 
  let mut cb_ref = PROSEMIRROR_CALLBACK.lock().unwrap();
