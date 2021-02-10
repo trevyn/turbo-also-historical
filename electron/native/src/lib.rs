c10895beb34a04cd6377941756a422f87cb8e29e
Eden <github@eden2.com>
2021-02-10 16:21:54 +0400

    wip insane futures thing

--- electron/native/src/lib.rs
+++ electron/native/src/lib.rs
@@ -1,22 +1,45 @@
+use futures::prelude::*;
 use neon::event::EventHandler;
 use neon::prelude::*;
 use once_cell::sync::Lazy;
-use std::sync::Mutex;
+use std::{future::Future, pin::Pin, sync::Mutex, task::Poll};
 
 #[allow(clippy::type_complexity)]
-static PROSEMIRROR_CALLBACK: Lazy<Mutex<Option<Box<dyn Fn() + Send>>>> =
- Lazy::new(|| Mutex::new(None));
+static PROSEMIRROR_CALLBACK: Lazy<
+ Mutex<Option<Box<dyn Fn(String, String) -> Pin<Box<dyn Future<Output = String>>> + Send>>>,
+> = Lazy::new(|| Mutex::new(None));
 
 #[allow(clippy::unnecessary_wraps)]
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
  if let Some(c) = PROSEMIRROR_CALLBACK.lock().unwrap().as_ref() {
   eprintln!("calling c()");
-  c();
+  c("10".to_string(), "20".to_string());
  } else {
   eprintln!("c not ready yet!");
  };
 
  Ok(cx.string("hello node 2"))
+}
+
+async fn do_thing() {
+ eprintln!("do thing");
+}
+
+// fn helper(cx: FunctionContext) -> JsResult<JsUndefined> {
+//  Ok(cx.undefined())
+// }
+
+struct MyFut;
+
+impl Future for MyFut {
+ type Output = String;
+ fn poll(self: Pin<&mut Self>, cx: &mut futures::task::Context) -> Poll<Self::Output> {
+  Poll::Ready("42".to_string())
+ }
+}
+
+fn my_fut() -> Pin<Box<dyn Future<Output = String>>> {
+ Box::pin(MyFut)
 }
 
 fn register_prosemirror_apply_callback(mut cx: FunctionContext) -> JsResult<JsUndefined> {
@@ -26,13 +49,19 @@ fn register_prosemirror_apply_callback(mut cx: FunctionContext) -> JsResult<JsUn
  let func = cx.argument::<JsFunction>(0)?;
  let cb = EventHandler::new(&cx, this, func);
 
- let execute = move || {
+ let execute = move |old: String, patch: String| -> Pin<Box<dyn Future<Output = String>>> {
+  // let mut output: Option<String> = None;
+
   cb.schedule(move |cx| {
    eprintln!("in scheduled");
-   let args: Vec<Handle<JsValue>> =
-    vec![cx.string("progress").upcast(), cx.string("1".to_string()).upcast()];
+
+   // let success = neon::types::JsFunction::new(&mut cx, helper);
+
+   let args: Vec<Handle<JsValue>> = vec![cx.string(old).upcast(), cx.string(patch).upcast()];
    args
-  })
+  });
+
+  Box::pin(MyFut)
  };
 
  let mut cb_ref = PROSEMIRROR_CALLBACK.lock().unwrap();
