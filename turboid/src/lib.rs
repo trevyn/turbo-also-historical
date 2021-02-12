084332c4ae1994a1b85141884e554fe12464e0ea
Eden <github@eden2.com>
2021-02-12 17:17:36 +0400

    add a quickcheck test

--- turboid/src/lib.rs
+++ turboid/src/lib.rs
@@ -4,4 +4,18 @@ mod tests {
  fn it_works() {
   assert_eq!(2 + 2, 4);
  }
+
+ fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+  let mut rev = vec![];
+  for x in xs {
+   rev.insert(0, x.clone())
+  }
+  rev
+ }
+
+ quickcheck::quickcheck! {
+  fn double_reversal_is_identity(xs: Vec<isize>) -> bool {
+   xs == reverse(&reverse(&xs))
+  }
+ }
 }
